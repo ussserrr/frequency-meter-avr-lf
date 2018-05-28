ad91fda11b13fa90ad3d8274707062639a005e4d
ussserrr <andrei4.2008@gmail.com>
2018-05-28 05:11:33 +0300

    README, comments

--- README.md
+++ README.md
@@ -1,12 +1,52 @@
 ## Overview
-Frequency meter firmware for ATmega328P MCU. It uses 16-bit Timer/Counter1 (TC1) as a main measuring part of the MCU.
+Frequency meter firmware for ATmega328P MCU. It uses 16-bit Timer/Counter1 (TC1) as a main measuring part of the MCU. Can be used as a standalone solution (no need in PC - LCD is used for indication). See [frequency-meter-avr-hf](https://github.com/ussserrr/frequency-meter-avr-hf) for measuring an HF band.
 
-Configured in "Input capture" mode, TC1 stores a number of internally generated pulses (with known timings) that had been counted during the one period of input signal, so we can find out its frequency as . This method is especially good for LF band as the accuracy there is a parameter of `N_pulses*period` value, so to improve it we should whether increase the internal frequency of TC1 (which we can't do in general) or increase the period of the measured signal (i.e. measure LF bands).
+```
+     __________________________                                     ____
+    /▉▉  ▉▉  ▉▉  ▉▉  ▉▉  ▉▉  ▉▉\ ▉▉  ▉▉  ▉▉  ▉▉  ▉▉  ▉▉  ▉▉  ▉▉  ▉▉/
+   / ▉▉  ▉▉  ▉▉  ▉▉  ▉▉  ▉▉  ▉▉ \▉▉  ▉▉  ▉▉  ▉▉  ▉▉  ▉▉  ▉▉  ▉▉  ▉▉
+__/__▉▉__▉▉__▉▉__▉▉__▉▉__▉▉__▉▉__▉▉__▉▉__▉▉__▉▉__▉▉__▉▉__▉▉__▉▉__▉▉_____
+      1   2   3   4   5   6   7   8   9  10  11  12  13  14  15 /16
+start on                           \                           /
+rising edge                         \                         /
+                                     -------------------------
+```
+Configured in "Input capture" mode, TC1 stores a number of internally generated pulses (with known timings) that had been counted during the one period of input signal (filling frequency), so we can find out its frequency as
+```
+             number_of_counted_ticks
+frequency = -------------------------
+                filling_frequency
+```
+This method is especially good for LF band as the accuracy there is a parameter of
+```
+accuracy ~ N_pulses * period
+```
+value, so to improve it we should whether increase the internal frequency of TC1 (which we can't do in general) or increase the period of the measured signal (i.e. measure LF bands). We also use an averaging to get more accurate results.
 
-For HF bands the right way is to use TC1 as a counter of an external signal' pulses. Measure out a 1-second interval (or any other known) by another available timer/counter (TC0 and TC2) and you can determine the frequency as simply as . In this case, the accuracy is proportional to `1/period` value so decreasing the period (i.e. increasing an input frequency) we get better results.
+The algorithm also detects when input frequency is rapidly changes (e.g. you tune it) and increases refresh rate of LCD indicating.
 
-Both methods are also use an averaging as an approach to improve the quality of the measuring.
+
+## Pinout
+Pin | Function
+--- | --------
+PB0 (Arduino's pin 8) | Input signal
+Arduino's 0, 1, 2, 3, 4, 6, 7 pins (respective PDx pins) | RS, RW, E, D4-7
 
 
 ## Build and run
-PlatformIO
+Build and run via [PlatformIO](https://platformio.org/). In `platformio.ini` specify parameters (e.g. F_CPU or programmer). Then run:
+```bash
+$ pio run  # build
+
+$ pio run -t program  # flash using external USBASP programmer
+or
+$ pio run -t upload  # flash using on-board programmer
+```
+
+
+## Limits and accuracy
+Check this [spreadsheet](https://docs.google.com/spreadsheets/d/1x5buIiSePPuyIJX-X4MWf-NA7JHJYz23IA_RD0JLFfs/edit?usp=sharing) to see some test measurements (second page). Generally, this method gives the most accurate results for input frequencies below 1 kHz. HF algorithm is more suited for all frequencies above that value.
+
+Also, you can replace crystal oscillator with another one (and even overclock a little bit) to increase maximal measurable frequency. In this case, remember to adjust F_CPU, Timer 0 and 1 clock sources for correct calculations and `board_build.f_cpu` in `platformio.ini` file.
+
+While TC1 is simply triggered by voltage level (rising or falling edge), MCU is able to measure not only strict square signals - sin wave, saw, triangle forms are also can be correctly recognized within certain limits of distortions.
--- src/FrequencyMeterAvrLF.cpp
+++ src/FrequencyMeterAvrLF.cpp
@@ -1,109 +1,157 @@
 #include <Arduino.h>
-#include <LiquidCrystal.h>
 
 #include <util/atomic.h>
 
 
+#include <LiquidCrystal.h>
 LiquidCrystal lcd(0, 1, 2, 3, 4, 6, 7);  // RS, RW (optional), E, D4-7
 #define BUFFER_SIZE 25
-char bufferA[BUFFER_SIZE];  // 1st raw of LCD
-// char bufferB[BUFFER_SIZE];  // 2nd raw of LCD
-// #define LCD_TIMER_PRESCALER 249
-uint8_t lcd_timer_prescaler_cnt = 0;
+char buffer[BUFFER_SIZE];  // buffer for raw of LCD
+
+
+/*
+ *  Accumulate number of pulses of Timer/Counter1' filling frequency in each one
+ *  period of input signal for averaging so
+ *
+ *                periods_accumulator
+ *    period = -------------------------
+ *              periods_accumulator_cnt
+ *
+ */
+uint32_t periods_accumulator = 0;
+uint32_t periods_accumulator_cnt = 0;
 
 double frequency;
-uint32_t period_accumulator = 0;
-uint32_t period_accumulator_cnt = 0;
-// bool is_first_measurement = true;
-
 double previous_frequency = 0.0;
-uint8_t timer2_additional_prescaler = 83;
-// uint8_t timer2_additional_prescaler_cnt = 0;
+
+/*
+ *  We use additional "prescaler" (integer variable) to get whether 1-second or
+ *  1/3-second updating period. Bigger period is used when input measured frequency
+ *  is stable while lesser one is for tuning mode (when input frequency changes rapidly)
+ */
+#define TIMER0_ADDITIONAL_PRESCALER_NORMAL_MODE 249
+#define TIMER0_ADDITIONAL_PRESCALER_TUNING_MODE 83
+uint8_t timer0_additional_prescaler = TIMER0_ADDITIONAL_PRESCALER_TUNING_MODE;
+uint8_t timer0_additional_prescaler_cnt = 0;
+/*
+ *  Changing in 1% of current frequency mean that user is tuning right now
+ *  so we need to increase refresh rate
+ */
 #define FREQUENCY_OFFSET 0.01
+
+/*
+ *  F_CPU divided by this give us the frequency of TC1 (i.e. filling frequency)
+ */
+#define FILLING_FREQUENCY_PRESCALER 256.0
 
 
 
 int main() {
-    cli();
 
-    lcd.begin(16, 2);
-	lcd.clear();
-	lcd.print("starting...");
+    ATOMIC_BLOCK(ATOMIC_FORCEON) {
 
-    /* Timer0 setup (LCD) */
-    TCCR0A |= (1<<WGM01);  // CTC mode
-    OCR0A = 250;
-    TCCR0B |= (1<<CS02);  // prescaler - 256
-    TIMSK0 |= (1<<OCIE0A);
+        lcd.begin(16, 2);
+    	lcd.clear();
+    	lcd.print("starting...");
 
-    /* Timer1 setup */
-    // // Input capture mode: rising edge
-    // TCCR1B |= (1<<ICES1);
-    // Noise canceler (4-cycles delay)
-    TCCR1B |= (1<<ICNC1);
-    // Input capture interrupt ON
-    TIMSK1 |= (1<<ICIE1);
-    // 256 precsaler and start timer
-    TCCR1B |= (1<<CS12);
+        /*
+         *  Timer0 setup (LCD refreshing)
+         */
+        TCCR0A |= (1<<WGM01);   // CTC mode
+        OCR0A = 250;
+        TIMSK0 |= (1<<OCIE0A);  // enable compare interrupt
+        TCCR0B |= (1<<CS02);    // prescaler FILLING_FREQUENCY_PRESCALER and start the timer
 
-    // DDRC |= (1<<PC2);
+        /*
+         *  Timer1 setup (Input Capture mode, on rising edges)
+         */
+        TCCR1B |= (1<<ICNC1);   // noise canceler (4-cycles delay)
+        TIMSK1 |= (1<<ICIE1);   // input capture interrupt ON
+        TCCR1B |= (1<<CS12);    // 256 precsaler and start the timer
 
-    sei();
+    }
 
     while (1) {}
 }
 
 
+
+/*
+ *  Accumulate number of pulses of Timer/Counter1' filling frequency in each one
+ *  period of input signal for averaging so
+ *
+ *                periods_accumulator
+ *    period = -------------------------
+ *              periods_accumulator_cnt
+ *
+ *
+ *        __________________________                                     ____
+ *       /▉▉  ▉▉  ▉▉  ▉▉  ▉▉  ▉▉  ▉▉\ ▉▉  ▉▉  ▉▉  ▉▉  ▉▉  ▉▉  ▉▉  ▉▉  ▉▉/
+ *      / ▉▉  ▉▉  ▉▉  ▉▉  ▉▉  ▉▉  ▉▉ \▉▉  ▉▉  ▉▉  ▉▉  ▉▉  ▉▉  ▉▉  ▉▉  ▉▉
+ *   __/__▉▉__▉▉__▉▉__▉▉__▉▉__▉▉__▉▉__▉▉__▉▉__▉▉__▉▉__▉▉__▉▉__▉▉__▉▉__▉▉_____
+ *         1   2   3   4   5   6   7   8   9  10  11  12  13  14  15 /16
+ *   start on                           \                           /
+ *   rising edge                         \                         /
+ *                                        -------------------------
+ *
+ *
+ *  On each interrupt fire, content of counting register (TCNT1) is copied into
+ *  ICR1. We also need to subtruct the time of ISR entering (in ticks) because
+ *  TCNT1 isn't cleared automatically
+ */
 ISR (TIMER1_CAPT_vect, ISR_NAKED) {
-    // cli();
+
     ATOMIC_BLOCK(ATOMIC_FORCEON) {
 
-    // if (is_first_measurement) {
-    //     is_first_measurement = false;
-    //     reti();
-    // }
+        TCNT1 -= ICR1;
+        periods_accumulator += ICR1;
+        periods_accumulator_cnt++;
 
-    TCNT1 -= ICR1;
-    period_accumulator += ICR1;
-    period_accumulator_cnt++;
     }
 
-    // sei();
     reti();
 }
 
 
+
+/*
+ *  Timer for LCD. Inside it we use additional "prescaler" (integer variable) to
+ *  get whether 1-second or 1/3-second updating period. Bigger period is used
+ *  when input measured frequency is stable while lesser one is for tuning mode
+ *  (when input frequency changes rapidly)
+ */
 ISR (TIMER0_COMPA_vect) {
-    // cli();
+
     ATOMIC_BLOCK(ATOMIC_FORCEON) {
 
-    if (++lcd_timer_prescaler_cnt == timer2_additional_prescaler) {
-        // PORTC ^= (1<<PC2);
-        lcd_timer_prescaler_cnt = 0;
+        // Additional prescaler so we display the measured frequency much less
+        // often (also averaging)
+        if (++timer0_additional_prescaler_cnt == timer0_additional_prescaler) {
+            timer0_additional_prescaler_cnt = 0;
 
-        frequency = (16000000.0f/256.0f)*((double)period_accumulator_cnt/period_accumulator);
-        if (frequency != frequency)
-            frequency = 0.0;
+            frequency = (((double)F_CPU)/FILLING_FREQUENCY_PRESCALER) *\
+                        (((double)periods_accumulator_cnt)/((double)periods_accumulator));
 
-        period_accumulator = 0;
-        period_accumulator_cnt = 0;
-        // is_first_measurement = true;
+            // NaN case (no input signal i.e. division by 0)
+            if (frequency != frequency)
+                frequency = 0.0;
 
-        if ( fabs(frequency-previous_frequency) > (previous_frequency*FREQUENCY_OFFSET) ) {
-            timer2_additional_prescaler = 83;
+            periods_accumulator = 0;
+            periods_accumulator_cnt = 0;
+
+            // If frequency is changed too often we assume that we are in the Tuning
+            // mode and switch to more frequent rate of LCD updates
+            if ( fabs(frequency-previous_frequency) > (previous_frequency*FREQUENCY_OFFSET) )
+                timer0_additional_prescaler = TIMER0_ADDITIONAL_PRESCALER_TUNING_MODE;
+            else
+                timer0_additional_prescaler = TIMER0_ADDITIONAL_PRESCALER_NORMAL_MODE;
+            previous_frequency = frequency;
+
+            lcd.clear();
+            sprintf(buffer, "%g Hz", frequency);
+            lcd.print(buffer);
         }
-        else {
-            timer2_additional_prescaler = 249;
-        }
-        previous_frequency = frequency;
 
-        lcd.clear();
-        sprintf(bufferA, "%g Hz", frequency);///1000.0f);
-        lcd.print(bufferA);
     }
 
-    // sei();
-    }
-
-    // reti();
 }
